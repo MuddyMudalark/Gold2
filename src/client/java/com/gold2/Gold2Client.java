f6c1022ab6d65597641f41d84a6702f476c93b6f
Morty <morty.ransom@gmail.com>
2024-11-24 13:54:23 +0000

    Initial commit

---
+++ src/client/java/com/gold2/Gold2Client.java
@@ -0,0 +1,10 @@
+package com.gold2;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Gold2Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/gold2/Gold2.java
@@ -0,0 +1,15 @@
+package com.gold2;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Gold2 implements ModInitializer {
+	public static final String MOD_ID = "gold2";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
